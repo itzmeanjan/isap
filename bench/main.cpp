c8e908239802d0078fd2a218706aa6da38836f0f
Anjan Roy <hello@itzmeanjan.in>
2022-06-23 17:02:49 +0400

    benchmark ISAP-K-128* AEAD
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,5 +1,7 @@
 #include "bench_isap_a_128.hpp"
 #include "bench_isap_a_128a.hpp"
+#include "bench_isap_k_128.hpp"
+#include "bench_isap_k_128a.hpp"
 
 // registering ISAP-A-128A encrypt/ decrypt routines for benchmark
 BENCHMARK(isap_bench::isap_a_128a_aead_encrypt)->Args({ 32, 64 });
@@ -32,6 +34,38 @@ BENCHMARK(isap_bench::isap_a_128_aead_encrypt)->Args({ 32, 2048 });
 BENCHMARK(isap_bench::isap_a_128_aead_decrypt)->Args({ 32, 2048 });
 BENCHMARK(isap_bench::isap_a_128_aead_encrypt)->Args({ 32, 4096 });
 BENCHMARK(isap_bench::isap_a_128_aead_decrypt)->Args({ 32, 4096 });
+
+// registering ISAP-K-128A encrypt/ decrypt routines for benchmark
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 64 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 64 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 128 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 128 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 256 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 256 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 512 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 512 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 1024 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 1024 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 2048 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 2048 });
+BENCHMARK(isap_bench::isap_k_128a_aead_encrypt)->Args({ 32, 4096 });
+BENCHMARK(isap_bench::isap_k_128a_aead_decrypt)->Args({ 32, 4096 });
+
+// registering ISAP-K-128 encrypt/ decrypt routines for benchmark
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 64 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 64 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 128 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 128 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 256 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 256 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 512 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 512 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 1024 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 1024 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 2048 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 2048 });
+BENCHMARK(isap_bench::isap_k_128_aead_encrypt)->Args({ 32, 4096 });
+BENCHMARK(isap_bench::isap_k_128_aead_decrypt)->Args({ 32, 4096 });
 
 // main function to drive execution of benchmark
 BENCHMARK_MAIN();
