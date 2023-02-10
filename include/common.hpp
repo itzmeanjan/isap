8477761e1052c0f268cc762b8be65db92b7f24fd
Anjan Roy <hello@itzmeanjan.in>
2023-02-10 12:55:31 +0400

    rewrite `ISAP_Mac` routine
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/common.hpp
+++ include/common.hpp
@@ -411,32 +411,14 @@ mac(const uint8_t* const __restrict key,
 
     uint64_t state[5]{};
 
-    state[0] = (static_cast<uint64_t>(nonce[0]) << 56) |
-               (static_cast<uint64_t>(nonce[1]) << 48) |
-               (static_cast<uint64_t>(nonce[2]) << 40) |
-               (static_cast<uint64_t>(nonce[3]) << 32) |
-               (static_cast<uint64_t>(nonce[4]) << 24) |
-               (static_cast<uint64_t>(nonce[5]) << 16) |
-               (static_cast<uint64_t>(nonce[6]) << 8) |
-               (static_cast<uint64_t>(nonce[7]) << 0);
+    std::memcpy(state, nonce, 16);
+    std::memcpy(state + 2, IV_A, sizeof(IV_A));
 
-    state[1] = (static_cast<uint64_t>(nonce[8]) << 56) |
-               (static_cast<uint64_t>(nonce[9]) << 48) |
-               (static_cast<uint64_t>(nonce[10]) << 40) |
-               (static_cast<uint64_t>(nonce[11]) << 32) |
-               (static_cast<uint64_t>(nonce[12]) << 24) |
-               (static_cast<uint64_t>(nonce[13]) << 16) |
-               (static_cast<uint64_t>(nonce[14]) << 8) |
-               (static_cast<uint64_t>(nonce[15]) << 0);
-
-    state[2] = (static_cast<uint64_t>(IV_A[0]) << 56) |
-               (static_cast<uint64_t>(IV_A[1]) << 48) |
-               (static_cast<uint64_t>(IV_A[2]) << 40) |
-               (static_cast<uint64_t>(IV_A[3]) << 32) |
-               (static_cast<uint64_t>(IV_A[4]) << 24) |
-               (static_cast<uint64_t>(IV_A[5]) << 16) |
-               (static_cast<uint64_t>(IV_A[6]) << 8) |
-               (static_cast<uint64_t>(IV_A[7]) << 0);
+    if constexpr (std::endian::native == std::endian::little) {
+      for (size_t i = 0; i < 3; i++) {
+        state[i] = bswap(state[i]);
+      }
+    }
 
     ascon::permute<s_h>(state);
 
@@ -449,27 +431,31 @@ mac(const uint8_t* const __restrict key,
 
       for (size_t i = 0; i < blk_cnt; i++) {
         const size_t off = i * rate;
-        const uint64_t w = (static_cast<uint64_t>(data[off + 0]) << 56) |
-                           (static_cast<uint64_t>(data[off + 1]) << 48) |
-                           (static_cast<uint64_t>(data[off + 2]) << 40) |
-                           (static_cast<uint64_t>(data[off + 3]) << 32) |
-                           (static_cast<uint64_t>(data[off + 4]) << 24) |
-                           (static_cast<uint64_t>(data[off + 5]) << 16) |
-                           (static_cast<uint64_t>(data[off + 6]) << 8) |
-                           (static_cast<uint64_t>(data[off + 7]) << 0);
 
-        state[0] ^= w;
+        uint64_t word;
+        std::memcpy(&word, data + off, sizeof(word));
+
+        if constexpr (std::endian::native == std::endian::little) {
+          word = bswap(word);
+        }
+
+        state[0] ^= word;
         ascon::permute<s_h>(state);
       }
 
       const size_t off = blk_cnt * rate;
-      uint64_t w = static_cast<uint64_t>(seperator) << ((7 - rm_bytes) << 3);
 
-      for (size_t i = 0; i < rm_bytes; i++) {
-        w |= static_cast<uint64_t>(data[off + i]) << ((7 - i) << 3);
+      uint64_t word = 0;
+      std::memcpy(&word, data + off, rm_bytes);
+
+      if constexpr (std::endian::native == std::endian::little) {
+        word |= static_cast<uint64_t>(seperator) << (rm_bytes * 8);
+        word = bswap(word);
+      } else {
+        word |= static_cast<uint64_t>(seperator) << ((7 - rm_bytes) * 8);
       }
 
-      state[0] ^= w;
+      state[0] ^= word;
       ascon::permute<s_h>(state);
 
       state[4] ^= 0b1; // seperator between associated data & cipher text
@@ -483,27 +469,31 @@ mac(const uint8_t* const __restrict key,
 
       for (size_t i = 0; i < blk_cnt; i++) {
         const size_t off = i * rate;
-        const uint64_t w = (static_cast<uint64_t>(cipher[off + 0]) << 56) |
-                           (static_cast<uint64_t>(cipher[off + 1]) << 48) |
-                           (static_cast<uint64_t>(cipher[off + 2]) << 40) |
-                           (static_cast<uint64_t>(cipher[off + 3]) << 32) |
-                           (static_cast<uint64_t>(cipher[off + 4]) << 24) |
-                           (static_cast<uint64_t>(cipher[off + 5]) << 16) |
-                           (static_cast<uint64_t>(cipher[off + 6]) << 8) |
-                           (static_cast<uint64_t>(cipher[off + 7]) << 0);
 
-        state[0] ^= w;
+        uint64_t word;
+        std::memcpy(&word, cipher + off, sizeof(word));
+
+        if constexpr (std::endian::native == std::endian::little) {
+          word = bswap(word);
+        }
+
+        state[0] ^= word;
         ascon::permute<s_h>(state);
       }
 
       const size_t off = blk_cnt * rate;
-      uint64_t w = static_cast<uint64_t>(seperator) << ((7 - rm_bytes) << 3);
 
-      for (size_t i = 0; i < rm_bytes; i++) {
-        w |= static_cast<uint64_t>(cipher[off + i]) << ((7 - i) << 3);
+      uint64_t word = 0;
+      std::memcpy(&word, cipher + off, rm_bytes);
+
+      if constexpr (std::endian::native == std::endian::little) {
+        word |= static_cast<uint64_t>(seperator) << (rm_bytes * 8);
+        word = bswap(word);
+      } else {
+        word |= static_cast<uint64_t>(seperator) << ((7 - rm_bytes) * 8);
       }
 
-      state[0] ^= w;
+      state[0] ^= word;
       ascon::permute<s_h>(state);
     }
     // --- end absorbing cipher text ---
@@ -512,98 +502,54 @@ mac(const uint8_t* const __restrict key,
 
     uint8_t y[knt_len];
     uint8_t skey[knt_len];
+    uint64_t tmp[2];
 
-    y[0] = static_cast<uint8_t>(state[0] >> 56);
-    y[1] = static_cast<uint8_t>(state[0] >> 48);
-    y[2] = static_cast<uint8_t>(state[0] >> 40);
-    y[3] = static_cast<uint8_t>(state[0] >> 32);
-    y[4] = static_cast<uint8_t>(state[0] >> 24);
-    y[5] = static_cast<uint8_t>(state[0] >> 16);
-    y[6] = static_cast<uint8_t>(state[0] >> 8);
-    y[7] = static_cast<uint8_t>(state[0] >> 0);
+    std::memcpy(tmp, state, sizeof(tmp));
+    if constexpr (std::endian::native == std::endian::little) {
+      tmp[0] = bswap(tmp[0]);
+      tmp[1] = bswap(tmp[1]);
+    }
 
-    y[8] = static_cast<uint8_t>(state[1] >> 56);
-    y[9] = static_cast<uint8_t>(state[1] >> 48);
-    y[10] = static_cast<uint8_t>(state[1] >> 40);
-    y[11] = static_cast<uint8_t>(state[1] >> 32);
-    y[12] = static_cast<uint8_t>(state[1] >> 24);
-    y[13] = static_cast<uint8_t>(state[1] >> 16);
-    y[14] = static_cast<uint8_t>(state[1] >> 8);
-    y[15] = static_cast<uint8_t>(state[1] >> 0);
+    std::memcpy(y, tmp, sizeof(tmp));
 
     rekeying<p, rk_flag_t::MAC, s_b, s_k, s_e, s_h>(key, y, skey);
 
-    state[0] = (static_cast<uint64_t>(skey[0]) << 56) |
-               (static_cast<uint64_t>(skey[1]) << 48) |
-               (static_cast<uint64_t>(skey[2]) << 40) |
-               (static_cast<uint64_t>(skey[3]) << 32) |
-               (static_cast<uint64_t>(skey[4]) << 24) |
-               (static_cast<uint64_t>(skey[5]) << 16) |
-               (static_cast<uint64_t>(skey[6]) << 8) |
-               (static_cast<uint64_t>(skey[7]) << 0);
-
-    state[1] = (static_cast<uint64_t>(skey[8]) << 56) |
-               (static_cast<uint64_t>(skey[9]) << 48) |
-               (static_cast<uint64_t>(skey[10]) << 40) |
-               (static_cast<uint64_t>(skey[11]) << 32) |
-               (static_cast<uint64_t>(skey[12]) << 24) |
-               (static_cast<uint64_t>(skey[13]) << 16) |
-               (static_cast<uint64_t>(skey[14]) << 8) |
-               (static_cast<uint64_t>(skey[15]) << 0);
+    std::memcpy(state, skey, sizeof(skey));
+    if constexpr (std::endian::native == std::endian::little) {
+      state[0] = bswap(state[0]);
+      state[1] = bswap(state[1]);
+    }
 
     ascon::permute<s_h>(state);
 
-    tag[0] = static_cast<uint8_t>(state[0] >> 56);
-    tag[1] = static_cast<uint8_t>(state[0] >> 48);
-    tag[2] = static_cast<uint8_t>(state[0] >> 40);
-    tag[3] = static_cast<uint8_t>(state[0] >> 32);
-    tag[4] = static_cast<uint8_t>(state[0] >> 24);
-    tag[5] = static_cast<uint8_t>(state[0] >> 16);
-    tag[6] = static_cast<uint8_t>(state[0] >> 8);
-    tag[7] = static_cast<uint8_t>(state[0] >> 0);
-
-    tag[8] = static_cast<uint8_t>(state[1] >> 56);
-    tag[9] = static_cast<uint8_t>(state[1] >> 48);
-    tag[10] = static_cast<uint8_t>(state[1] >> 40);
-    tag[11] = static_cast<uint8_t>(state[1] >> 32);
-    tag[12] = static_cast<uint8_t>(state[1] >> 24);
-    tag[13] = static_cast<uint8_t>(state[1] >> 16);
-    tag[14] = static_cast<uint8_t>(state[1] >> 8);
-    tag[15] = static_cast<uint8_t>(state[1] >> 0);
+    std::memcpy(tmp, state, sizeof(tmp));
+    if constexpr (std::endian::native == std::endian::little) {
+      tmp[0] = bswap(tmp[0]);
+      tmp[1] = bswap(tmp[1]);
+    }
+    std::memcpy(tag, tmp, sizeof(tmp));
 
     // --- end squeezing tag ---
 
-  } else if (p == perm_t::KECCAK) {
+  } else {
     // --- begin initialization ---
 
     uint16_t state[25]{};
 
+    std::memcpy(state, nonce, 16);
+    std::memcpy(state + 8, IV_A, sizeof(IV_A));
+
+    if constexpr (std::endian::native == std::endian::big) {
 #if defined __clang__
-#pragma unroll 8
+#pragma clang loop unroll(enable)
+#pragma clang loop vectorize(enable)
 #elif defined __GNUG__
 #pragma GCC ivdep
-#pragma GCC unroll 8
+#pragma GCC unroll 12
 #endif
-    for (size_t i = 0; i < 8; i++) {
-      const size_t noff = i << 1;
-
-      state[i] = (static_cast<uint16_t>(nonce[noff ^ 1]) << 8) |
-                 (static_cast<uint16_t>(nonce[noff ^ 0]) << 0);
-    }
-
-    constexpr size_t soff = 8;
-
-#if defined __clang__
-#pragma unroll 4
-#elif defined __GNUG__
-#pragma GCC ivdep
-#pragma GCC unroll 4
-#endif
-    for (size_t i = 0; i < 4; i++) {
-      const size_t ivoff = i << 1;
-
-      state[soff + i] = (static_cast<uint16_t>(IV_A[ivoff ^ 1]) << 8) |
-                        (static_cast<uint16_t>(IV_A[ivoff ^ 0]) << 0);
+      for (size_t i = 0; i < 12; i++) {
+        state[i] = bswap(state[i]);
+      }
     }
 
     keccak::permute<s_h>(state);
@@ -612,40 +558,35 @@ mac(const uint8_t* const __restrict key,
 
     // --- begin absorbing associated data ---
     {
-      const size_t blk_cnt = dlen / rate;
-      const size_t rm_bytes = dlen % rate;
+      size_t off = 0;
+      while (off < dlen) {
+        const size_t elen = std::min(rate, dlen - off);
 
-      for (size_t i = 0; i < blk_cnt; i++) {
-        const size_t off = i * rate;
+        for (size_t i = 0; i < elen; i += 2) {
+          uint16_t mword = 0;
+          std::memcpy(&mword, data + off + i, std::min(elen - i, 2ul));
 
-        for (size_t j = 0; j < rate; j += 2) {
-          const size_t soff = j >> 1;
-          const uint16_t w = (static_cast<uint16_t>(data[off + j + 1]) << 8) |
-                             (static_cast<uint16_t>(data[off + j + 0]) << 0);
+          if constexpr (std::endian::native == std::endian::big) {
+            mword = bswap(mword);
+          }
 
-          state[soff] ^= w;
+          state[i / 2] ^= mword;
         }
 
-        keccak::permute<s_h>(state);
+        off += elen;
+
+        if (elen == rate) [[likely]] {
+          keccak::permute<s_h>(state);
+        }
       }
 
-      const size_t off = blk_cnt * rate;
-
-      for (size_t i = 0; i < rm_bytes; i++) {
-        const size_t soff = i >> 1;
-        const size_t boff = (i & 1) << 3;
-
-        const uint16_t w = static_cast<uint16_t>(data[off + i]) << boff;
-
-        state[soff] ^= w;
-      }
-
+      const size_t rm_bytes = dlen % rate;
       const size_t soff = rm_bytes >> 1;
-      const size_t boff = (rm_bytes & 1) << 3;
+      const size_t boff = (rm_bytes & 1) * 8;
 
       const uint16_t w = static_cast<uint16_t>(seperator) << boff;
-
       state[soff] ^= w;
+
       keccak::permute<s_h>(state);
 
       state[24] ^= 0b1 << 8; // seperator between associated data & cipher text
@@ -654,40 +595,35 @@ mac(const uint8_t* const __restrict key,
 
     // --- begin absorbing cipher text ---
     {
-      const size_t blk_cnt = clen / rate;
-      const size_t rm_bytes = clen % rate;
+      size_t off = 0;
+      while (off < clen) {
+        const size_t elen = std::min(rate, clen - off);
 
-      for (size_t i = 0; i < blk_cnt; i++) {
-        const size_t off = i * rate;
+        for (size_t i = 0; i < elen; i += 2) {
+          uint16_t mword = 0;
+          std::memcpy(&mword, cipher + off + i, std::min(elen - i, 2ul));
 
-        for (size_t j = 0; j < rate; j += 2) {
-          const size_t soff = j >> 1;
-          const uint16_t w = (static_cast<uint16_t>(cipher[off + j + 1]) << 8) |
-                             (static_cast<uint16_t>(cipher[off + j + 0]) << 0);
+          if constexpr (std::endian::native == std::endian::big) {
+            mword = bswap(mword);
+          }
 
-          state[soff] ^= w;
+          state[i / 2] ^= mword;
         }
 
-        keccak::permute<s_h>(state);
+        off += elen;
+
+        if (elen == rate) [[likely]] {
+          keccak::permute<s_h>(state);
+        }
       }
 
-      const size_t off = blk_cnt * rate;
-
-      for (size_t i = 0; i < rm_bytes; i++) {
-        const size_t soff = i >> 1;
-        const size_t boff = (i & 1) << 3;
-
-        const uint16_t w = static_cast<uint16_t>(cipher[off + i]) << boff;
-
-        state[soff] ^= w;
-      }
-
+      const size_t rm_bytes = clen % rate;
       const size_t soff = rm_bytes >> 1;
-      const size_t boff = (rm_bytes & 1) << 3;
+      const size_t boff = (rm_bytes & 1) * 8;
 
       const uint16_t w = static_cast<uint16_t>(seperator) << boff;
-
       state[soff] ^= w;
+
       keccak::permute<s_h>(state);
     }
     // --- end absorbing cipher text ---
@@ -697,29 +633,56 @@ mac(const uint8_t* const __restrict key,
     uint8_t y[knt_len];
     uint8_t skey[knt_len];
 
+    if constexpr (std::endian::native == std::endian::little) {
+      std::memcpy(y, state, sizeof(y));
+    } else {
 #if defined __clang__
-#pragma unroll 16
+#pragma clang loop unroll(enable)
+#pragma clang loop vectorize(enable)
 #elif defined __GNUG__
 #pragma GCC ivdep
 #pragma GCC unroll 16
 #endif
-    for (size_t i = 0; i < knt_len; i++) {
-      y[i] = static_cast<uint8_t>(state[i >> 1] >> ((i & 1) << 3));
+      for (size_t i = 0; i < knt_len; i++) {
+        y[i] = static_cast<uint8_t>(state[i >> 1] >> ((i & 1) << 3));
+      }
     }
 
     rekeying<p, rk_flag_t::MAC, s_b, s_k, s_e, s_h>(key, y, skey);
 
-    for (size_t i = 0; i < 8; i++) {
-      const size_t skoff = i << 1;
+    if constexpr (std::endian::native == std::endian::little) {
+      std::memcpy(state, skey, sizeof(skey));
+    } else {
+#if defined __clang__
+#pragma clang loop unroll(enable)
+#pragma clang loop vectorize(enable)
+#elif defined __GNUG__
+#pragma GCC ivdep
+#pragma GCC unroll 8
+#endif
+      for (size_t i = 0; i < 8; i++) {
+        const size_t skoff = i << 1;
 
-      state[i] = (static_cast<uint16_t>(skey[skoff ^ 1]) << 8) |
-                 (static_cast<uint16_t>(skey[skoff ^ 0]) << 0);
+        state[i] = (static_cast<uint16_t>(skey[skoff + 1]) << 8) |
+                   (static_cast<uint16_t>(skey[skoff + 0]) << 0);
+      }
     }
 
     keccak::permute<s_h>(state);
 
-    for (size_t i = 0; i < knt_len; i++) {
-      tag[i] = static_cast<uint8_t>(state[i >> 1] >> ((i & 1) << 3));
+    if constexpr (std::endian::native == std::endian::little) {
+      std::memcpy(tag, state, 16);
+    } else {
+#if defined __clang__
+#pragma clang loop unroll(enable)
+#pragma clang loop vectorize(enable)
+#elif defined __GNUG__
+#pragma GCC ivdep
+#pragma GCC unroll 16
+#endif
+      for (size_t i = 0; i < knt_len; i++) {
+        tag[i] = static_cast<uint8_t>(state[i >> 1] >> ((i & 1) << 3));
+      }
     }
 
     // --- end squeezing tag ---
